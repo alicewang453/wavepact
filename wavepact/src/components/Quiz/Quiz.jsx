d007e7f0c311a82d88bf1217e4dede2aac745804
williamyzhang <80303545+williamyzhang@users.noreply.github.com>
2023-12-16 21:33:27 -0500

    added a quiz template

---
+++ wavepact/src/components/Quiz/Quiz.jsx
@@ -0,0 +1,373 @@
+import { useState, useEffect, useRef } from "react";
+import { resultInitialState } from "../../constants";
+import "./Quiz.scss";
+
+const Quiz = ({ questions }) => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answerIdx, setAnswerIdx] = useState(null);
+    const [answerChoice, setAnswerChoice] = useState(null);
+    const [answerPersonality, setAnswerPersonality] = useState(null);
+    const [result, setResult] = useState(resultInitialState);
+    const [showResult, setShowResult] = useState(false);
+
+    const [audioContext, setAudioContext] = useState(null);
+    // const [whiteNoiseBuffer, setWhiteNoiseBuffer] = useState(null);
+    const [source, setSource] = useState(null);
+    const [playing, setPlaying] = useState(false);
+    const [currSound, setCurrSound] = useState(null);
+
+    const [globalGain, setGlobalGain] = useState(null);
+
+    const canvasRef = useRef(null);
+    const [analyser, setAnalyser] = useState(null);
+    const [animationId, setAnimationId] = useState(null);
+
+    // Initialize AudioContext & GlobalGain
+    useEffect(() => {
+        const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        setAudioContext(audioCtx);
+        const gg = audioCtx.createGain();
+        gg.gain.setValueAtTime(0.8, audioCtx.currentTime);
+        gg.connect(audioCtx.destination);
+        setGlobalGain(gg);
+        console.log('creating audio context & global gain')
+        const globalAnalyser = audioCtx.createAnalyser();
+        gg.connect(globalAnalyser);
+        // globalAnalyser.connect(audioCtx.destination)
+        setAnalyser(globalAnalyser);
+        console.log('created analyser');
+        draw();
+        return () => {
+            if (audioContext) {
+                audioContext.close();
+            }
+        };
+    }, []);
+
+    // useEffect(() => {
+    //     return () => {
+    //         cancelAnimationFrame(animationId);
+    //     };
+    // }, [animationId]);
+
+    const draw = () => {
+        if (!analyser) return;
+        
+        var WIDTH = 300;
+        var HEIGHT = 300;
+
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+        analyser.getByteTimeDomainData(dataArray);
+    
+        var canvas = canvasRef.current;
+        var canvasCtx = canvas.getContext("2d");
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+        var drawVisual = requestAnimationFrame(draw);
+
+        analyser.getByteFrequencyData(dataArray);
+
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        var barWidth = (WIDTH / bufferLength) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] * 2;
+
+            canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+            canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+
+            x += barWidth + 1;
+        }
+
+        // requestAnimationFrame(draw);
+
+        // analyser.getByteTimeDomainData(dataArray);
+
+        // canvasCtx.fillStyle = "white";
+        // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // canvasCtx.lineWidth = 2;
+        // canvasCtx.strokeStyle = "rgb(31,117,254)";
+
+        // canvasCtx.beginPath();
+
+        // var sliceWidth = canvas.width * 1.0 / bufferLength;
+        // var x = 0;
+
+        // for (var i = 0; i < bufferLength; i++) {
+        //     var v = dataArray[i] / 128.0;
+        //     var y = v * canvas.height / 2;
+        //     if (i === 0) {
+        //         canvasCtx.moveTo(x, y);
+        //     } else {
+        //         canvasCtx.lineTo(x, y);
+        //     }
+        //     x += sliceWidth;
+        // }
+
+        // canvasCtx.lineTo(canvas.width, canvas.height / 2);
+        // canvasCtx.stroke();
+    };
+
+    // Generic function to play a given noise type
+    const playNoise = (noiseBuffer, noiseType) => {
+        if (source) {
+            source.stop();
+            setSource(null);
+        }
+        const noiseGain = audioContext.createGain();
+        noiseGain.gain.setValueAtTime(0.3, audioContext.currentTime);
+        noiseGain.connect(globalGain);
+
+        let noiseSource = audioContext.createBufferSource();
+        noiseSource.buffer = noiseBuffer;
+        noiseSource.loop = true;
+        noiseSource.connect(noiseGain);
+
+        noiseSource.connect(analyser);
+
+        noiseSource.start();
+        setSource(noiseSource);
+        setPlaying(true);
+        setCurrSound(noiseType);
+
+
+        // // Create or reuse the analyser node
+        // let analyserNode = analyser;
+        // if (!analyserNode) {
+        //     analyserNode = audioContext.createAnalyser();
+        //     analyserNode.fftSize = 2048;
+        //     setAnalyser(analyserNode);
+        // }
+
+        // // Connect the nodes in the correct order
+        // noiseSource.connect(analyserNode);
+        // analyserNode.connect(audioContext.destination);
+
+        // noiseSource.start();
+        // setSource(noiseSource);
+
+        // requestAnimationFrame(draw);
+    };
+
+    // Functions for generating different types of noise
+    const generateNoise = (noiseType) => {
+        const bufferSize = 2 * audioContext.sampleRate;
+        // console.log('buffer size: ', bufferSize);
+        const noiseBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
+        const output = noiseBuffer.getChannelData(0);
+
+        // Fill buffer with noise data based on noiseType
+        switch (noiseType) {
+            case 'White Noise':
+                for (let i = 0; i < bufferSize; i++) {
+                    output[i] = Math.random() * 2 - 1;
+                }
+                break;
+            case 'Pink Noise':
+                // Pink noise algorithm
+                let b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0;
+                for (let i = 0; i < bufferSize; i++) {
+                    let white = Math.random() * 2 - 1;
+                    b0 = 0.99886 * b0 + white * 0.0555179;
+                    b1 = 0.99332 * b1 + white * 0.0750759;
+                    b2 = 0.96900 * b2 + white * 0.1538520;
+                    b3 = 0.86650 * b3 + white * 0.3104856;
+                    b4 = 0.55000 * b4 + white * 0.5329522;
+                    b5 = -0.7616 * b5 - white * 0.0168980;
+                    output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+                    output[i] *= 0.11; // Compensate for gain
+                    b6 = white * 0.115926;
+                }
+                break;
+            case 'Brown Noise':
+                // Brown noise algorithm
+                let lastOut = 0.0;
+                for (let i = 0; i < bufferSize; i++) {
+                    let white = Math.random() * 2 - 1;
+                    output[i] = (lastOut + (0.02 * white)) / 1.02;
+                    lastOut = output[i];
+                    output[i] *= 3.5; // Compensate for gain
+                }
+                break;
+            case 'Blue Noise':
+                // Simplified blue noise algorithm
+                for (let i = 0; i < bufferSize; i++) {
+                    // Generate white noise
+                    let whiteNoise = Math.random() * 2 - 1;
+    
+                    // Apply a simple high-frequency emphasis
+                    let scaleFactor = i / bufferSize;
+                    output[i] = whiteNoise * scaleFactor;
+                }
+                break;
+            default:
+                break;
+        }
+
+        playNoise(noiseBuffer, noiseType);
+    };
+
+    const handlePlay = (noiseType) => {
+        if (playing) {
+            source.stop();
+            if (currSound === noiseType) {
+                setPlaying(false);
+                setCurrSound(null);
+            } else {
+                generateNoise(noiseType);
+            }
+        } else {
+            generateNoise(noiseType);
+        }
+    };
+
+    const { question, choices, answerVals, type} = questions[currentQuestion]; 
+    
+    const onAnswerClick = (answer, index, answerVals) => {
+        // console.log(answer);
+        // console.log(index);
+        setAnswerIdx(index);
+        setAnswerChoice(answer);
+        console.log(answerChoice);
+        setAnswerPersonality(answerVals[answer]);
+        console.log('answer personality: ', answerPersonality);
+        // console.log('curr result: ', result);
+        // if (answer === correctAnswer) {
+        //     setAnswer(true);
+        // } else {
+        //     setAnswer(false);
+        // }
+    }
+
+    const onClickNext = () => {
+        if (playing) {
+            handlePlay(currSound);
+        } 
+        setAnswerIdx(null);
+        console.log('Selected: ', answerChoice)
+        console.log('prev result: ', result);
+        var newResult = {
+            IE: result.IE + answerPersonality.IE,
+            SN: result.SN + answerPersonality.SN,
+            TF: result.TF + answerPersonality.TF,
+            JP: result.JP + answerPersonality.JP,
+            AT: result.AT + answerPersonality.AT, 
+        }
+        setResult(newResult);
+        console.log('new result: ', newResult);
+        //     prev.IE += answerPersonality.IE;
+        //     prev.SN += answerPersonality.SN;
+        //     prev.TF += answerPersonality.TF;
+        //     prev.JP += answerPersonality.JP;
+        //     prev.AT += answerPersonality.AT;
+        // }
+        // );
+
+        if (currentQuestion !== questions.length - 1) {
+            setCurrentQuestion((prev) => prev + 1);
+        } else {
+            setCurrentQuestion(0);
+            setShowResult(true);
+        }
+    }
+
+    const onTryAgain = () => {
+        setResult(resultInitialState);
+        setShowResult(false);
+    };
+
+    // const handleInputChange = () => {
+    //     setInputAnswer
+    // }
+
+    const getAnswerUI = () => {
+        if (type === "noise") {
+            return (
+                <ul>
+                    {
+                        choices.map((choice, index) => (
+                            <li
+                                onClick={function(event){
+                                    onAnswerClick(choice, index, answerVals);
+                                    handlePlay(choice);
+                                    }
+                                }
+                                key={choice}
+                                className={answerIdx === index ? 'selected-answer' : null}
+                            >
+                                {choice}
+                            </li>
+                        ))
+                    }
+                </ul>)
+        }
+
+        return (
+        <ul>
+            {
+                choices.map((choice, index) => (
+                    <li
+                        onClick={function(event){
+                            onAnswerClick(choice, index, answerVals);
+                            // togglePlay();
+                            }
+                        }
+                        key={choice}
+                        className={answerIdx === index ? 'selected-answer' : null}
+                    >
+                        {choice}
+                    </li>
+                ))
+            }
+        </ul>)
+    }
+
+    return (
+        <div className="quiz-container">
+            {!showResult ? (<>
+            <span className="active-question-num">{currentQuestion + 1}</span>
+            <span className="total-question">/{questions.length}</span>
+            <h2>{question}</h2>
+            {getAnswerUI()}
+            <div className="footer">
+                <button onClick={onClickNext} disabled={answerIdx === null}>
+                    {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+                </button>
+            </div>
+            <div class="container">
+                <canvas ref={canvasRef} width="300" height="170">
+                wave form visualizer
+                </canvas>
+            </div>
+        </>) : <div className="result">
+            <h3>Result</h3>
+            <p>
+                Introversion/Extraversion: <span>{result.IE}</span>
+            </p>
+            <p>
+                Observant/Intuitive: <span>{result.SN}</span>
+            </p>
+            <p>
+                Thinking/Feeling: <span>{result.TF}</span>
+            </p>
+            <p>
+                Judging/Prospecting: <span>{result.JP}</span>
+            </p>
+            <p>
+                Assertive/Turbulent: <span>{result.AT}</span>
+            </p>
+            <button onClick={onTryAgain}>Play again</button>  {/* change to share results */}
+            </div>}
+        
+        </div>
+    );
+};
+
+export default Quiz;
--- wavepact/src/constants.js
+++ wavepact/src/constants.js
@@ -1,51 +1,120 @@
 export const jsQuizz = {
     questions: [
-      {
-        question:
-          "Which of the following is used in React.js to increase performance?",
-        choices: [
-          "Virtual DOM",
-          "Original DOM",
-          "Both A and B",
-          "None of the above",
-        ],
-        type: "MCQs",
-        correctAnswer: "Virtual DOM",
-      },
-      {
-        question: "What is ReactJS?",
-        choices: [
-          "Server-side framework",
-          "User Interface framework",
-          "both a and b",
-          "None of the above",
-        ],
-        type: "MCQs",
-        correctAnswer: "User Interface framework",
-      },
-      {
-        question:
-          "Identify the one which is used to pass data to components from outside",
-        choices: ["Render with arguments", "setState", "PropTypes", "props"],
-        type: "MCQs",
-        correctAnswer: "props",
-      },
-      {
-        question: "In which language is React.js written?",
-        choices: ["Python", "Java", "C#", "JavaScript"],
-        type: "MCQs",
-        correctAnswer: "JavaScript",
-      },
-      {
-        question: "What is Babel?",
-        choices: [
-          "JavaScript interpreter",
-          "JavaScript transpiler",
-          "JavaScript compiler",
-          "None of the above",
-        ],
-        type: "MCQs",
-        correctAnswer: "JavaScript compiler",
-      },
+        {
+            question:
+              "Which noise do you prefer? (Click each to hear)",
+            choices: [
+              "White Noise",
+              "Pink Noise",
+              "Brown Noise",
+              "Blue Noise",
+            ],
+            type: "noise",
+            answerVals: {
+              "White Noise": {IE: 0, SN: 0, TF: 0, JP: 0, AT: 2},
+              "Pink Noise": {IE: 0, SN: 0, TF: 0, JP: 0, AT: -2},
+              "Brown Noise": {IE: 0, SN: 0, TF: 0, JP: 0, AT: -5},
+              "Blue Noise": {IE: 0, SN: 0, TF: 0, JP: 0, AT: 5},
+            },
+          },
+        {
+            question:
+              "What sound effect do you like best?",
+            choices: [
+              "Babbling Brook",
+              "Muttering Motor",
+            ],
+            type: "sound",
+            answerVals: {
+              "Babbling Brook": {IE: 0, SN: 0, TF: 5, JP: 5, AT: 0},
+              "Muttering Motor": {IE: 0, SN: 0, TF: -5, JP: -5, AT: 0},
+            },
+          },
+        {
+            question:
+              "Choose your favorite waveform!",
+            choices: [
+              "Sine",
+              "Sawtooth",
+              "Square",
+              "Triangle",
+            ],
+            type: "sound",
+            answerVals: {
+              "Sine": {IE: 0, SN: 5, TF: 0, JP: 0, AT: 0},
+              "Sawtooth": {IE: 0, SN: -2, TF: 0, JP: 0, AT: 0},
+              "Square": {IE: 0, SN: -5, TF: 0, JP: 0, AT: 0},
+              "Triangle": {IE: 0, SN: 2, TF: 0, JP: 0, AT: 0},
+            },
+          },
+          {
+            question:
+              "Watch the live coding clips and pick the one you like better!",
+            choices: [
+              "Silly Sonic Pi",
+              "Tomfoolerous Tidal Cycles",
+            ],
+            type: "video",
+            answerVals: {
+              "Silly Sonic Pi": {IE: 5, SN: 0, TF: 0, JP: 0, AT: 0},
+              "Tomfoolerous Tidal Cycles": {IE: -5, SN: 0, TF: 0, JP: 0, AT: 0},
+            },
+          },
+    //     {
+    //     question:
+    //       "Which of the following is used in React.js to increase performance?",
+    //     choices: [
+    //       "Virtual DOM",
+    //       "Original DOM",
+    //       "Both A and B",
+    //       "None of the above",
+    //     ],
+    //     type: "MCQs",
+    //     correctAnswer: "Virtual DOM",
+    //   },
+    //   {
+    //     question: "What is ReactJS?",
+    //     choices: [
+    //       "Server-side framework",
+    //       "User Interface framework",
+    //       "both a and b",
+    //       "None of the above",
+    //     ],
+    //     type: "MCQs",
+    //     correctAnswer: "User Interface framework",
+    //   },
+    //   {
+    //     question:
+    //       "Identify the one which is used to pass data to components from outside",
+    //     choices: ["Render with arguments", "setState", "PropTypes", "props"],
+    //     type: "MCQs",
+    //     correctAnswer: "props",
+    //   },
+    //   {
+    //     question: "In which language is React.js written?",
+    //     choices: ["Python", "Java", "C#", "JavaScript"],
+    //     type: "MCQs",
+    //     correctAnswer: "JavaScript",
+    //   },
+    //   {
+    //     question: "What is Babel?",
+    //     choices: [
+    //       "JavaScript interpreter",
+    //       "JavaScript transpiler",
+    //       "JavaScript compiler",
+    //       "None of the above",
+    //     ],
+    //     type: "MCQs",
+    //     correctAnswer: "JavaScript compiler",
+    //   },
     ],
   };
+
+  export const resultInitialState = {
+    //MBTI: negative = trait on left, positive = trait on right
+    IE: 0, //introversion - extraversion
+    SN: 0, //observant - intuitive
+    TF: 0, //thinking - feeling
+    JP: 0, //judging - prospecting
+    AT: 0, //assertive - turbulent
+  }
