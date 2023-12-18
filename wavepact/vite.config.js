1541f9895ee05a5970c4819bfeded126f9dd87aa
williamyzhang <80303545+williamyzhang@users.noreply.github.com>
2023-12-18 00:36:20 -0500

    config updates

--- wavepact/vite.config.js
+++ wavepact/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/16waves",
-  plugins: [react([
-    'react-wavepact/src/main.jsx'
-  ])],
+  plugins: [react()],
 })
